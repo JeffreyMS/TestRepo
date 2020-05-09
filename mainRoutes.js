e2066f9d54ef9bc1307fbe169d1a0374996aba31
JeffreyMS <jeffreym887@gmail.com>
2020-05-09 02:53:30 +0200

    Lab3 exercise

---
+++ mainRoutes.js
@@ -0,0 +1,17 @@
+let path = require('path');
+let express = require('express');
+let mainRouter = express.Router();
+
+mainRouter.get('/', function (req, res) {
+    res.send('Hello World, I\'m Node.js');
+    });
+
+mainRouter.get('/',function (req, res){
+    res.send('My name is Shudufhadzo');
+});
+
+mainRouter.get('/about',function (req, res){
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+module.exports = mainRouter;
