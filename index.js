41cac828f6ac61b0c0e1b9d93b286ba3fdab581a
JeffreyMS <jeffreym887@gmail.com>
2020-05-09 04:08:09 +0200

    port changed to 80

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ let mainRouter = require("./mainRoutes");
 app.use(mainRouter);
 
 let port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(80);
 console.log("Express server running on port", port);
